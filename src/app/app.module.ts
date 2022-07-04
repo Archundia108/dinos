9e92fb29e42e0567f6358c529a738edfdc806d0b
MooshroomBear <alexisdm.ti19@utsjr.edu.mx>
2022-07-04 18:40:56 -0500

    Página editar-dinos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InsertarDinosComponent } from './pages/insertar-dinos/insertar-dinos.component';
 import { ListarDinosComponent } from './pages/listar-dinos/listar-dinos.component';
+import { EditarDinosComponent } from './pages/editar-dinos/editar-dinos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InsertarDinosComponent,
-    ListarDinosComponent
+    ListarDinosComponent,
+    EditarDinosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/editar-dinos/editar-dinos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-dinos',
+  templateUrl: './editar-dinos.component.html',
+  styleUrls: ['./editar-dinos.component.css']
+})
+export class EditarDinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
