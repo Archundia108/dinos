b4106dd5a498fc6bc821022735492a29f799ab4c
Archundia108 <gabrielaa.ti19@utsjr.edu.mx>
2022-07-07 16:42:30 -0500

    Insertar editar

--- src/app/pages/editar-dinos/editar-dinos.component.ts
+++ src/app/pages/editar-dinos/editar-dinos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Dino } from 'src/app/models/dino';
+import { DinoService } from 'src/app/services/dino.service';
 
 @Component({
   selector: 'app-editar-dinos',
@@ -7,9 +11,114 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarDinosComponent implements OnInit {
 
-  constructor() { }
+  //propiedades
+  enviado = false;
+  tipo: any = ['Carvinovoro', 'Herbivoro', 'Omnivoro', 'Piscivoro']
+  genero: any = ['Tiranosauridae', 'Spinosauridae', 'Abelisauridae', 'Ceratopsidae', 'Carcharodontosauridae']
+  editarForm: FormGroup;
+  empleadoData: Dino[];
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private dinoService: DinoService,
+    private actRouter: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.mainForm();
+    let id = this.actRouter.snapshot.paramMap.get('id');
+    this.getDino(id);
+    this.editarForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
+      tamanio: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+      peso: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+    });
+  }
+
+  mainForm(){
+    this.editarForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
+      tamanio: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+      peso: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+    });
+  }
+
+  //método para seleccionar un tipo con un select
+  actualizarTipoDino(d){
+    this.editarForm.get('tipo').setValue(d, {
+      onlySelf: true,
+    });
+  }
+
+  //método para seleccionar un genero con un select
+  actualizarGeneroDino(d){
+    this.editarForm.get('genero').setValue(d, {
+      onlySelf: true,
+    });
+  }
+
+  //getter para acceder a los controles del formulario
+  get myForm(){
+    return this.editarForm.controls
+  }
+
+  //método para buscar al empleado que vamos a modificar
+  getDino(id){
+    this.dinoService.getDino(id).subscribe((data) => {
+      this.editarForm.setValue({
+        nombre: data['nombre'],
+        tipo: data['tipo'],
+        genero: data['genero'],
+        tamanio: data['tamanio'],
+        peso: data['peso'],
+      });
+    });
+  }
+
+  //método que se ejecuta cuanndo se envia el formulario
+  onSubmit(){
+    this.enviado = true;
+    if(!this.editarForm.valid){
+      return false;
+    }else{
+      if(window.confirm('¿Estás seguro de editar el dinosaurio?')){
+        let id = this.actRouter.snapshot.paramMap.get('id');
+        this.dinoService.updateDino(id, this.editarForm.value).subscribe({
+          complete: () => {
+            this.router.navigateByUrl('/listar-dinos');
+            console.log('El dinosaurio se actualizó correctamente');
+          },
+          error: (e) => {
+            console.log(e);
+          },
+        });
+      }
+    }
   }
 
 }
--- src/app/pages/insertar-dinos/insertar-dinos.component.ts
+++ src/app/pages/insertar-dinos/insertar-dinos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DinoService } from 'src/app/services/dino.service';
 
 @Component({
   selector: 'app-insertar-dinos',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertarDinosComponent implements OnInit {
 
-  constructor() { }
+  //propiedades
+  enviado = false;
+  tipo: any = ['Carvinovoro', 'Herbivoro', 'Omnivoro', 'Piscivoro']
+  genero: any = ['Tiranosauridae', 'Spinosauridae', 'Abelisauridae', 'Ceratopsidae', 'Carcharodontosauridae']
+  insertarForm: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private ngZone: NgZone,
+    private router: Router,
+    private dinoService: DinoService
+  ) {
+    this.mainForm();
+  }
 
   ngOnInit(): void {
   }
 
+  mainForm(){
+    this.insertarForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
+      tamanio: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+      peso: ['', 
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$'),
+        ],
+      ],
+    });
+  }
+
+  //método para seleccionar un tipo con un select
+  actualizarTipoDino(d){
+    this.insertarForm.get('tipo').setValue(d, {
+      onlySelf: true,
+    });
+  }
+
+  //método para seleccionar un genero con un select
+  actualizarGeneroDino(d){
+    this.insertarForm.get('genero').setValue(d, {
+      onlySelf: true,
+    });
+  }
+
+  //getter para acceder a los controles del formulario
+  get myForm(){
+    return this.insertarForm.controls
+  }
+
+  //método que se ejecuta cuanndo se envia el formulario
+  onSubmit(){
+    this.enviado = true;
+    if(!this.insertarForm.valid){
+      return false;
+    }else{
+      return this.dinoService.agregarDino(this.insertarForm.value).subscribe({
+        complete: () => {
+          console.log('Dinosaurio agregado correctamente'),
+          this.ngZone.run(() => this.router.navigateByUrl('/listar-dinos'));
+        },
+        error: (e) => {
+          console.log(e);
+        },
+      });
+    }
+  }
 }
