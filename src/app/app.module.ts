6b67582368d2e10d3c0c62e3dec69552f06ba777
EmmanuelCruzCh117 <emmanuel.cruz18@hotmail.com>
2022-07-07 17:41:26 -0500

    Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { InsertarDinosComponent } from './pages/insertar-dinos/insertar-dinos.component';
 import { ListarDinosComponent } from './pages/listar-dinos/listar-dinos.component';
 import { EditarDinosComponent } from './pages/editar-dinos/editar-dinos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { EditarDinosComponent } from './pages/editar-dinos/editar-dinos.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
